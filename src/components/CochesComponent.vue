dfd80c8858d983e53fbc52326418fb433deff975
Jorgerdzz <jorgerodriguezalonso20@gmail.com>
2025-10-30 12:34:01 +0100

    primer servicio Api

---
+++ src/components/CochesComponent.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <h1>Api Coches</h1>
+        <div v-for="coche in coches" :key="coche">
+            <h3 style="color:blue">{{coche.marca}} {{coche.modelo}}</h3>
+            <p>Driver: {{coche.conductor}}</p>
+            <img :src="coche.imagen" style="width: 150px; height:150px"/>
+        </div>
+    </div>
+</template>
+
+<script>
+import axios from 'axios';
+let urlApi = "https://apicochespaco.azurewebsites.net/"
+
+    export default{
+        name: "CochesComponent",
+        data(){
+            return{
+                coches:[]
+            }
+        },
+        mounted(){
+            this.loadCoches();
+        },
+        methods:{
+            loadCoches(){
+                let request = "webresources/coches";
+                axios.get(urlApi + request).then(response=>{
+                    this.coches = response.data;
+                })
+            }
+        }
+    }
+</script>
