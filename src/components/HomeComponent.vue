9b32eb0b38cefc406547ce993900cb2ad00390ae
Jorgerdzz <jorgerodriguezalonso20@gmail.com>
2025-10-31 10:01:36 +0100

    Implementación de la clase service

--- src/components/HomeComponent.vue
+++ src/components/HomeComponent.vue
@@ -1,13 +1,23 @@
 <template>
     <div>
         <h1>Servicios Api</h1>
+        <h2 style="color:blue">{{mensaje}}</h2>
     </div>
 </template>
 
 <script>
-
+import ServiceEjemplo from '../services/ServiceEjemplo';
+const serviceEjemplo = new ServiceEjemplo()
     export default {
         name: "CustomersComponent",
+        data(){
+            return{
+                mensaje: ""
+            }
+        },
+        mounted(){
+            this.mensaje = serviceEjemplo.getSaludo("Jorge");
+        }
     }
 
 </script>
